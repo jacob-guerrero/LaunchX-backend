7ad1273beaf7437749f5121a6ffeb7b21829f505
Jacob <104036951+j31c0@users.noreply.github.com>
2022-12-01 03:23:22 -0500

    Agregar contenido clase y gitignore

--- node-and-express/index.js
+++ node-and-express/index.js
@@ -1,10 +1,32 @@
-// Proceso asincrono
-/* http = require('http');
+let express = require('express');
+let app = express();
 
-console.log("Hola");
-http.get('http://httpstat.us/200', (res) => {
-    console.log(`La API devolvio: ${res.statusCode}`);
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+// Create
+app.post('/', function(req, res) {
+    res.send(`Te llamas ${req.body.name}`);
 });
 
-console.log("Adios"); */
+// Read
+app.get('/', function(req, res) {
+    res.send("Hola mundo");
+});
 
+// Update
+app.put('/', function(req, res) {
+    res.send(`Haz actualizado el registro numero: ${req.body.id}`);
+});
+
+// Delete
+app.delete('/users', function(req, res) {
+    res.send(`Haz eliminado el registro: ${req.query.user}`)
+});
+
+let server = app.listen(8080, function () {
+    let host = server.address().address;
+    let port = server.address().port;
+
+    console.log("Aplicacion escuchando en http://%s:%s", host, port);
+})
