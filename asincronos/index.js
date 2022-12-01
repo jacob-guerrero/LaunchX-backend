9f58871518cabcca3eb320d81c872810c2110e9a
Jacob <104036951+j31c0@users.noreply.github.com>
2022-12-01 03:23:59 -0500

    Cambiar nombre a carpeta

---
+++ asincronos/index.js
@@ -0,0 +1,13 @@
+// Proceso asincrono
+/* http = require('http');
+
+console.log("Hola");
+http.get('http://httpstat.us/200', (res) => {
+    console.log(`La API devolvio: ${res.statusCode}`);
+});
+
+console.log("Adios"); */
+
+setTimeout(() => {
+    console.log("Me haz esperado <3");
+}, 2000);
---
+++ asincronos/santapromesa.js
@@ -0,0 +1,41 @@
+// Con promises
+function leerCarta(carta) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(carta.split("").reverse().join(""));
+        }, 2000);
+    });
+
+}
+
+function construirJuguete (instruccion) {
+    return new Promise((resolve, reject) => {
+        setTimeout (() => {
+        const juguete = instruccion.split("").reverse().join("");
+        if (juguete.includes("madera"))
+            return resolve (`${juguete} de madera fina`);
+        else if (juguete.includes("xbox"))
+            return resolve (`No te puedo mandar tu ${juguete}`);
+        else
+            return resolve (`Aqui tienes tu cosa esta ${juguete}`);
+        }, 5000);
+    });
+}
+
+function envolverRegalo (juguete) {
+    return new Promise ((resolve, reject) => {
+        
+        setTimeout(() => {
+        resolve(`${juguete} envuelto`);
+        }, 3000)
+    });
+}
+
+leerCarta ("xbox Series X")
+    .then ((instruccion) => {
+        return construirJuguete (instruccion);
+    })
+    .then ((juguete) => {
+        return envolverRegalo (juguete);
+    })
+    .then (console.log)
