6b625d75c1a60d9781d88c64b079d4033df65bab
Jacob <104036951+j31c0@users.noreply.github.com>
2022-12-01 01:34:02 -0500

    Agregar proceso asincrono

---
+++ node-and-express/index.js
@@ -0,0 +1,10 @@
+// Proceso asincrono
+/* http = require('http');
+
+console.log("Hola");
+http.get('http://httpstat.us/200', (res) => {
+    console.log(`La API devolvio: ${res.statusCode}`);
+});
+
+console.log("Adios"); */
+
